6594ddfe9c8d47b310557c61c8871edb5432bf8a
tyassin95-lgtm <tyassin95@gmail.com>
2025-11-22 15:33:07 +0000

    Changed token regeneration to 14 days

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PANEL_BANNER_URL: 'https://i.imgur.com/JhdFffS.png', // replace with your banner
   DEFAULT_DB_NAME: 'guildhelper',
-  TOKEN_REGENERATION_DAYS: 7, // Days until token regenerates after being used
+  TOKEN_REGENERATION_DAYS: 14, // Days until token regenerates after being used
   DEBOUNCE_DELAY_MS: 1200, // Delay for live summary updates
   USER_ACTION_COOLDOWN_MS: 1000 // Cooldown between user actions (anti-spam)
 };
